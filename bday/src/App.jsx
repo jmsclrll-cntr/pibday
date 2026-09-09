db1c491155ceba98af12cb99e2be53df9f97fc0a
ertnac <cantrejames33@gmail.com>
2026-09-09 21:00:38 +0800

    asd

--- bday/src/App.jsx
+++ bday/src/App.jsx
@@ -10,15 +10,15 @@ function App() {
         subText="Sorry kung ganito lang actually kanina kolang na-code to HAHAHHAHHA,
          busy kasi ako nitong mga nakaraang araw, sana appreciate mo"
         title="Happy Birthday! 🎉"
-        message="Ginawa pang gift kala mo mukang regalo e no, HAHAHAHAHAHHAHHA! 
-        Hii aliii, happy happy birthday, you are the one who make me laugh make 
-        me happy and also make me sad, in a way that we dont talked like before, 
-        i wish this day makes very special to you and i wish na magawa mo lahat 
-        ng mga gusto mo ngayong year, at sana hindi magbago yung ali na masayahin, 
-        laging kumakanta (remix payan sya), palatawa at pala dura (yuck HAHAHAHAHWHHWW) 
-        at wag lagi mainit ang ulo. Kasi miss na miss ko yung dating ikaw 😔😞. 
-        Wala nako masabi speechless kasi ako sayo HAHAHAHAHA, yun lang, see you
-         soonnnnnn, imissyouuusomuch, mwaaaaaa!❤️"
+        message="Ginawa pang gift kala mo mukang regalo e no? , HAHAHAHAHAHHAHHA!
+                Hii aliii, happy happy birthday, to the one who make me laugh, make
+                me happy, and also make me sad, in a way that we dont talked like before, palagi parin bumabalik sakin kung pano tayo nagkakilala napapangiti nalang talaga kapag naaalala koyon HAHAHABHAHAHAHA andami dami pang plano plano, drawing yarn? Joke kidding aside,
+                i wish this day makes very special to you and i wish na magawa mo lahat
+                ng mga gusto mo this year, at sana hindi magbago yung ali na masayahin,
+                mahilig kumakanta (remix payan sya), palatawa at pala dura (yuck HAHAHAHAHWHHWW)
+                at wag palagi mainit ang ulo ganyan talaga kapag mag ttrentahin na pinaka batang menopause HAHAHAHAHAHA. miss na miss ko yung dating ikaw, dating tayo 😔😞.
+                Wala nako masabi speechless kasi ako sayo HAHAHAHAHA, yun lang, i hope to see you
+                 soonnnnn, imissyouuusomuch, mwaaaaaa!❤️"
         senderName="With love & hugs ❤️"
       />
     </main>
